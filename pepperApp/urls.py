45f25ee8d9011df5e6c61780a8828b5ad7de436b
Tim Andrews <tim.andrews@imagineifinc.com>
2016-03-11 09:52:08 -0500

    Something broke with link, names etc.
    In the interest of moving forward I’m doing it the non-dynamic way for
    now.

--- pepperApp/urls.py
+++ pepperApp/urls.py
@@ -5,7 +5,7 @@ import pepperApp.views
 
 
 urlpatterns = [
-    url(r'^$', pepperApp.views.testDb_list, name='testDb'),
+    url(r'^$', pepperApp.views.testDb_list),
     url(r'^create/$', pepperApp.views.testDb_create),
     url(r'^(?P<id>\d)/$', pepperApp.views.testDb_detail, name='detail'),
     url(r'^update/$', pepperApp.views.testDb_update),
